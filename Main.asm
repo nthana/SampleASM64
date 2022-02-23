2239b170e6b309d26396978679926c66c9986973
THANA NITAYARERK <thana_nita@yahoo.com>
2022-02-23 09:43:25 +0700

    Add Full Prolog & Epilog code

--- Main.asm
+++ Main.asm
@@ -8,9 +8,12 @@ myCaption		db "Message Box Testing", 0
 
 .code
 main PROC
-	sub rsp, 28h		; reserved as Win32 parameters' shadow area
-						; see: Microsoft's X64 Calling Convention document
-						; https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention?view=msvc-170
+						; usual x86-64 prolog code
+	push rbp			;	save caller's rbp
+	mov rbp, rsp		;	set rbp to point to start of stack frame
+	sub rsp, 20h		;	reserved as Win32 parameters' shadow area
+						;	see: Microsoft's X64 Calling Convention document
+						;	https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention?view=msvc-170
 	mov rcx, 0
 	lea rdx, myText
 	lea r8, myCaption
@@ -21,6 +24,11 @@ main PROC
 	mov rcx, 12345678	; the exit code
 	call ExitProcess	; see: Win32 ExitProcess document
 						; https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-exitprocess
+
+						; usual x86-64 epilog code
+	mov rsp, rbp		;	restore stack pointer
+	pop rbp				;	restore old (caller's) rbp
+	ret
 main ENDP
 
 END
